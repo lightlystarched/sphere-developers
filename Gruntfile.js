c74dc3769123307d10e5a1ccd8fdd0644bac55ac
Josh Martin <agile.gibbon@gmail.com>
2015-04-15 14:48:15 -0400

    minor updates to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
         },
         src: [
           'scripts/**/*.js',
-          'images/**',
+          'images/*',
           'styles/*.css'
         ],
         dest: '<%= yeoman.dist %>/sphere.appcache'
